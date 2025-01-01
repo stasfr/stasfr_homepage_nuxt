80929e9551228be65ae0c83d2fcb0943f7a4213c
stas_fr <sfworking@yandex.ru>
2025-01-01 17:42:22 +0300

    chore: install nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode'
+  ]
 })
